c3c2687c4c81bc04a61998155f2596cf0428030e
suba-sris <subasrilazy098@gmail.com>
2023-10-27 18:55:54 +0530

    version-6

--- project-5/clock.js
+++ project-5/clock.js
@@ -16,3 +16,49 @@ console.log(sec)
 console.log(sec)
 }
 /* setInterval(toDay,1000) */
+
+const h = document.getElementById("hr")
+const m = document.getElementById("mn")
+const s = document.getElementById("sc")
+
+
+setInterval(()=>{
+
+    let day = new Date()
+    let hour = day.getHours() *30 ;
+    let min = day.getMinutes() * 6;
+   let sec = day.getSeconds()*6;
+
+    h.style.transform = `rotateZ(${hour + (min/12)}deg)`;
+   m.style.transform = `rotateZ(${min}deg)`;
+   s.style.transform = `rotateZ(${sec}deg)`;
+})
+
+// DIGITAL CLOCK
+
+const dh = document.getElementById("hour")
+const dm = document.getElementById("min")
+const ds = document.getElementById("secont")
+
+
+
+
+
+
+setInterval(() => {
+
+    let day = new Date()
+    let hour = day.getHours() 
+    let sec = day.getSeconds()
+     let min = day.getMinutes()
+
+      dh.innerHTML =v(hour)
+      dm.innerHTML =v(min) 
+      ds.innerHTML =v(sec)
+
+      
+}, );
+
+let v =(timeFormet)=>{
+  return timeFormet<10?"0"+timeFormet:timeFormet
+}
